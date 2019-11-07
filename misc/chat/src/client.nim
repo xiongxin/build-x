ece5edb0aab3a074102c224b3981f08c15a4dc84
xiongxin <1781189926@qq.com>
2019-11-07 15:38:32 +0800

    chat application complete

--- misc/chat/src/client.nim
+++ misc/chat/src/client.nim
@@ -1,4 +1,16 @@
-import os,threadpool
+import os,threadpool, asyncdispatch, asyncnet
+import protocol
+
+proc connect(socket: AsyncSocket, serverAddr: string) {.async.} =
+    echo "Connecting to ", serverAddr
+    await socket.connect(serverAddr, 7687.Port)
+    echo "Connected!"
+
+    while true:
+        let line = await socket.recvLine()
+        let parsed = parseMessage(line)
+        echo parsed.username, " said ", parsed.message
+
 
 echo "Chat application statred"
 
@@ -7,7 +19,19 @@ if paramCount() == 0:
 
 let serverAddr = paramStr(1)
 echo "Connecting to ", serverAddr
+var messageFlowVar = spawn stdin.readLine()
+var socket = newAsyncSocket()
+
+asyncCheck socket.connect(serverAddr)
 
 while true:
-    let message = spawn stdin.readLine()
-    echo "Sending, \"", ^message, "\""
+    if messageFlowVar.isReady():
+        let message = createMessage("Anonymous", ^messageFlowVar)
+        asyncCheck socket.send(message)
+        messageFlowVar = spawn stdin.readLine()
+    asyncdispatch.poll()
+
+# 项目优化点
+# 1. 允许输入用户名
+# 2. 实现客户端存活的监测 ping 命令
+# 3. 强制用户下线
