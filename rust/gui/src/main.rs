a1fa3b1a6d397c9b3a275bcdc814c05c1bfb62cf
xiongxin <1781189926@qq.com>
2020-04-29 10:01:39 +0800

    code

--- rust/gui/src/main.rs
+++ rust/gui/src/main.rs
@@ -1,3 +1,25 @@
+use gui::{Button, Screen, SelectBox};
+
+
 fn main() {
-    println!("Hello, world!");
+    let screen = Screen {
+        components: vec![
+            Box::new( SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No")
+                ]
+            }),
+            Box::new( Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK")
+            } )
+        ]
+    };
+
+    screen.run();
 }
