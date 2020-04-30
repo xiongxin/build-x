805a19f0fcd46c46f92c3188fb9f66146b10b361
xiongxin <1781189926@qq.com>
2020-04-30 09:05:43 +0800

    add threadpool

--- rust/hello/src/bin/main.rs
+++ rust/hello/src/bin/main.rs
@@ -3,12 +3,17 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::fs;
 
+use hello::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(10);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        pool.execute( || {
+            handle_connection(stream);
+        })
     }
 }
 
