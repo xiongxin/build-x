8e29fa95e0065dec09e194cf825c4dafb18f096a
xiongxin <1781189926@qq.com>
2020-05-06 12:46:54 +0800

    list

--- rust/lists/src/bin/main.rs
+++ rust/lists/src/bin/main.rs
@@ -1,3 +1,10 @@
+use lists::first::List;
+
 fn main() {
-  
+  let mut list = List::new();
+
+  list.push(12);
+  list.push(13);
+
+  println!("{:?}", list);
 }
--- rust/lists/src/first.rs
+++ rust/lists/src/first.rs
@@ -1,12 +1,40 @@
-pub type List = Option<Node>;
+#[derive(Debug)]
+pub struct List {
+  head: Link
+}
+#[derive(Debug)]
+enum Link {
+  Empty,
+  More(Box<Node>)
+}
 
+#[derive(Debug)]
 struct Node {
   elem: i32,
-  next: List
+  next: Link
 }
 
 impl List {
   pub fn new() -> Self {
-    None
+    List { head: Link::Empty }
+  }
+
+  pub fn push(&mut self, elem: i32) {
+    let new_node = Node {
+      elem,
+      next: std::mem::replace(&mut self.head, Link::Empty)
+    };
+
+    self.head = Link::More(Box::new(new_node));
+  }
+
+  pub fn pop(&mut self) -> Option<i32> {
+    match std::mem::replace(&mut self.head, Link::Empty) {
+      Link::Empty => None,
+      Link::More(node) => {
+        self.head = node.next;
+        Some(node.elem)
+      }
+    }
   }
 }
