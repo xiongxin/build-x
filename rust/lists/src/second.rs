bfc0592af472097fe259b0a6312c8589b852a88a
xiongxin <1781189926@qq.com>
2020-05-14 23:52:38 +0800

    codes

--- rust/lists/src/second.rs
+++ rust/lists/src/second.rs
@@ -54,6 +54,48 @@ impl<T> Iterator for IntoIter<T> {
   }
 }
 
+pub struct Iter<'a, T> {
+  next: Option<&'a Node<T>>,
+}
+
+impl<'a, T> List<T> {
+  pub fn iter(&self) -> Iter<T> {
+    Iter { next: self.head.as_ref().map(|node| &**node) }
+  }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+  type Item = &'a T;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    self.next.map(|node| {
+      self.next = node.next.as_ref().map(|node| &**node);
+      &node.elem
+    })
+  }
+}
+
+pub struct IterMut<'a, T> {
+  next: Option<&'a mut Node<T>>,
+}
+
+impl<'a, T> List<T> {
+  pub fn iter_mut(&mut self) -> IterMut<T> {
+    IterMut { next: self.head.as_mut().map(|node| &mut **node) } // 3
+  }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+  type Item = &'a mut T;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    self.next.take().map(|node| {
+        self.next = node.next.as_mut().map(|node| &mut **node); // self.next = 2
+        &mut node.elem
+    })
+  }
+}
+
 
 impl<T> Drop for List<T> {
   fn drop(&mut self) {
@@ -106,4 +148,18 @@ mod test {
     assert_eq!(iter.next(), Some(1));
     assert_eq!(iter.next(), None);
   }
+
+  #[test]
+    fn iter_mut() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.peek(), Some(&2));
+    }
 }
