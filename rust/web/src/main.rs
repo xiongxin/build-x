0ab17fce3915f1d9f454b4d471dd237e18a955e1
xiongxin <1781189926@qq.com>
2020-05-17 14:22:06 +0800

    rust web

---
+++ rust/web/src/main.rs
@@ -0,0 +1,34 @@
+use actix_web::{ web, App, HttpResponse, HttpServer, Responder, get };
+use listenfd::ListenFd;
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+async fn index2() -> impl Responder {
+    HttpResponse::Ok().body("Hello world again!")
+}
+
+#[get("/hello")]
+async fn index3() -> impl Responder {
+    HttpResponse::Ok().body("Hey 雄心波波!!!!")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    let mut listenfd = ListenFd::from_env();
+    let mut server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/again", web::get().to(index2))
+            .service(index3)
+    });
+
+    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
+        server.listen(l)?
+    } else {
+        server.bind("127.0.0.1:3000")?
+    };
+
+    server.run().await
+}
